40cc547d0f4a8dbcdefe6698f0dd91f4d3a4b102
Jevin <99015714+jevin08@users.noreply.github.com>
2022-09-18 21:54:16 +0530

    Add files via upload

---
+++ lab9_tutorial2/pages/choose_location.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class ChooseLocation extends StatefulWidget {
+// const ChooseLocation({Key? key}) : super(key: key);
+  @override
+  State<ChooseLocation> createState() => _ChooseLocationState();
+}
+
+class _ChooseLocationState extends State<ChooseLocation> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueGrey[200],
+      appBar: AppBar(
+        backgroundColor: Colors.deepOrange[200],
+        title: Text('CHOOSE LOCATION'),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: Text(
+        'CHOOSE LOCATION SCREEN',
+        style: TextStyle(fontSize: 20),
+      ),
+    );
+  }
+}
