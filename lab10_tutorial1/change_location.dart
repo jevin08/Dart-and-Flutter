6dc32ffb1cfe742ca9465072fa5b8ffdd5b43b81
Jevin <99015714+jevin08@users.noreply.github.com>
2022-09-19 00:49:43 +0530

    Create change_location.dart

---
+++ lab10_tutorial1/change_location.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+class ChooseLocation extends StatefulWidget {
+// const ChooseLocation({Key? key}) : super(key: key);
+  @override
+  State<ChooseLocation> createState() => _ChooseLocationState();
+}
+class _ChooseLocationState extends State<ChooseLocation> {
+  int counter = 0;
+  @override
+  Widget build(BuildContext context) {
+// print('BUILD FUNCTION RUN IN CHOOSE LOCATION...');
+    return Scaffold(
+      backgroundColor: Colors.blueGrey[200],
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurpleAccent,
+        title: Text('CHHOSE LOCATION'),
+        centerTitle: true,
+        elevation: 0,
+      ),
+    );
+  }
+}
