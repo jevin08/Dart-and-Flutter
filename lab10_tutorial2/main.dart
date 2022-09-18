51e33fa1bad17aa1e99bbe2ab3f7cb037e66f015
Jevin <99015714+jevin08@users.noreply.github.com>
2022-09-19 00:51:54 +0530

    Create main.dart

---
+++ lab10_tutorial2/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
+
+import 'package:lab7/world_time.dart';
+void main() => runApp(MaterialApp(
+// home: Home(),
+// instead of making home: property to make any page to initialize at beginning...
+// we can use following code ....
+// initialRoute: '/home',
+    initialRoute: '/',
+    routes: {
+      '/': (context) => Loading(),
+    }
+));
+class Loading extends StatefulWidget {
+  @override
+  State<Loading> createState() => _LoadingState();
+}
+class _LoadingState extends State<Loading> {
+  String? time = 'LOADING..........';
+  void setWorldTime() async {
+    WordTime timeinstance =
+    WordTime(location: 'kolkata',flag: 'india.png',url: 'Asia/Kolkata');
+    await timeinstance.getTime();
+// print(timeinstance.time);
+    setState(() {
+      time = timeinstance.time;
+    });
+  }
+  @override
+  void initState() {
+    super.initState();
+    setWorldTime();
+  }
+  @override
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Padding(
+          padding: EdgeInsets.all(60.0),
+          child: Text(time.toString()),
+        )
+    );
+  }
+}
