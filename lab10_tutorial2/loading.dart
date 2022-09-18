1e5a445ef7fa3d9d0aa381ad6182ba44522381f0
Jevin <99015714+jevin08@users.noreply.github.com>
2022-09-19 00:52:50 +0530

    Create loading.dart

---
+++ lab10_tutorial2/loading.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:lab10_2/world_time.dart';
+class Loading extends StatefulWidget {
+  @override
+  State<Loading> createState() => _LoadingState();
+}
+class _LoadingState extends State<Loading> {
+  String? time = 'LOADING..........';
+  void setWorldTime() async {
+    WordTime timeinstance =
+    WordTime(location: 'kolkata',flag: 'india.png',url: 'Asia/Kolkata');
+    await timeinstance.getTime();
+// print(timeinstance.time);
+    setState(() {
+      time = timeinstance.time;
+    });
+  }
+  @override
+  void initState() {
+    super.initState();
+    setWorldTime();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Padding(
+          padding: EdgeInsets.all(60.0),
+          child: Text(time.toString()),
+        )
+    );
+  }
+}
