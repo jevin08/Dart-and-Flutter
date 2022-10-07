038be26c9df024da80473c0902d60423504b2e09
Jevin <99015714+jevin08@users.noreply.github.com>
2022-10-07 09:51:32 +0530

    Add files via upload

---
+++ lab11_tutorial1/loading.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:lab11_tutorial1/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class Loading extends StatefulWidget {
+  @override
+  State<Loading> createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+  void setWorldTime() async {
+    WordTime timeInstance =
+        WordTime(location: 'kolkata', flag: 'india.png', url: 'Asia/Kolkata');
+    await timeInstance.getTime();
+    // Navigator.pushNamed(context, '/home');
+    Navigator.pushReplacementNamed(context, '/home', arguments: {
+      'location': timeInstance.location,
+      'flag': timeInstance.flag,
+      'time': timeInstance.time,
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setWorldTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.deepPurpleAccent,
+      body: SpinKitFadingCircle(
+        color: Colors.white,
+        size: 50.0,
+      ),
+    );
+  }
+}
